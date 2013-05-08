9f9eb8df11bbe7b538c18503c2132e9d333327f3
witooh <witoo@outlook.com>
2013-05-08 15:45:56 +0700

    fix static user_call_func

--- src/Witooh/MenuBuilder/MenuBuilder.php
+++ src/Witooh/MenuBuilder/MenuBuilder.php
@@ -1,27 +1,31 @@
 <?php
 namespace Witooh\MenuBuilder;
 
-class MenuBuilder {
+class MenuBuilder
+{
 
-    protected static function generate($menuBehavior, $configs, $ul, $menuLevel){
-        foreach($configs as $config){
-            $li = call_user_func("$menuBehavior::makeLI", array($config, $menuLevel));
-            if($li != false){
-                if(isset($config['menu']) && is_array($config['menu'])){
+    protected static function generate($menuBehavior, $configs, $ul, $menuLevel)
+    {
+        foreach ($configs as $config) {
+            $li = call_user_func(array($menuBehavior, "makeLI"), $config, $menuLevel);
+            if ($li != false) {
+                if (isset($config['menu']) && is_array($config['menu'])) {
                     $menuLevel++;
-                    $ulSubMenu = call_user_func("$menuBehavior::makeUL", $menuLevel);
+                    $ulSubMenu = call_user_func(array($menuBehavior, "makeUL"), $menuLevel);
                     $li->innerHtml(static::generate($menuBehavior, $config['menu'], $ulSubMenu, $menuLevel));
                     $ul->innerHtml($li);
-                }else{
+                } else {
                     $ul->innerHtml($li);
                 }
             }
         }
+
         return $ul;
     }
 
-    public static function make(array $configs, $menuBehavior='Witooh\Menu\Menu'){
-        $ul = call_user_func("$menuBehavior::makeUL", 0);
+    public static function make(array $configs, $menuBehavior = 'Witooh\MenuBuilder\Menu')
+    {
+        $ul = call_user_func(array($menuBehavior, "makeUL"), 0);
         $ul = static::generate($menuBehavior, $configs, $ul, 0);
         echo $ul->toString();
     }
